a1ebf439273ce53167473ae2c89d8f746e613463
guojiaMM <rena63350@gmail.com>
2016-07-26 15:36:04 -0700

    redirect logout to login

--- grader/main/urls.py
+++ grader/main/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     url(r'^homepage/$', views.homepage, name='homepage'),
     url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
     url(r'^registration/$', views.registration, name='registration'),
-    url(r'^logout/$', auth_views.logout, {'next_page': '/successfully_logged_out/'}, name='logout'),
+    url(r'^logout/$', auth_views.logout, {'next_page': '/login/'}, name='logout'),
     url(r'^successfully_logged_out/$', views.successfully_logged_out, name='successfully_logged_out')
     ]
