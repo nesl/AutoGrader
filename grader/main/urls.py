c0be247c4db0ecc518fa16f83e95be21afaeca4e
Bharathan Balaji <bbalaji@ucla.edu>
2016-07-26 18:38:53 -0700

    fixed login error messages

--- grader/main/urls.py
+++ grader/main/urls.py
@@ -3,6 +3,7 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    url(r'^$', views.homepage, name='homepage'),
     url(r'^login/$', auth_views.login, {'template_name': 'main/login.html'}, name= 'login'),
     url(r'^homepage/$', views.homepage, name='homepage'),
     url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
