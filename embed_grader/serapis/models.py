a8d441421832b6aa6dd44462b6c21637cce86877
Bharathan Balaji <bbalaji@ucla.edu>
2016-08-01 17:27:15 -0700

    adding a few models

--- embed_grader/serapis/models.py
+++ embed_grader/serapis/models.py
@@ -3,3 +3,27 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class HardwareTester(models.Model):
+    STATUS_TYPES = (
+        ('reserved','Reserved'),
+        ('avail', 'Available'),
+    )
+    status = models.CharField(
+        max_length=10,
+        choices=STATUS_TYPES,
+        default='avail',
+    )
+
+    TESTER_TYPES = (
+        ('beagle', 'BeagleBone'),
+        ('rpi3', 'RaspberryPi3')
+    )
+    type = models.CharField(
+        max_length = 10,
+        choices = TESTER_TYPES,
+        default = 'beagle'
+    )
+
+class HardwareTestBench(models.Model):
+    
