36a377b0995acd22047ca7331f9c45fd65342c2b
Bo-Jhang <TimeStringAlpha@gmail.com>
2016-08-17 15:25:41 -0700

    Show user name on the top
    - Also reformat to make it look better

--- embed_grader/serapis/views.py
+++ embed_grader/serapis/views.py
@@ -4,8 +4,29 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout, context_processors
 from django.template import RequestContext
 
+from serapis.models import *
 
-# Create your views here.
+# import the logging library
+import logging, logging.config
+import sys
+"""
+LOGGING = {
+        'version': 1,
+        'handlers': {
+            'console': {
+                'class': 'logging.StreamHandler',
+                'stream': sys.stdout,
+                }
+            },
+        'root': {
+            'handlers': ['console'],
+            'level': 'INFO'
+            }
+        }
+# Get an instance of a logger
+logging.config.dictConfig(LOGGING)
+"""
+logger = logging.getLogger(__name__)
 
 def index(request):
     return HttpResponse("Hello World!")
@@ -19,6 +40,10 @@ def login_user(request):
         password = request.POST['password']
 
         myuser = authenticate(username=uid, password=password)
+        #print('myuser')
+        #print(myuser)
+        #print("fatal error", file=sys.stderr)
+        logger.error('myuser')
         if myuser is not None:
             if myuser.is_active:
                 login(request, myuser)
@@ -27,15 +52,18 @@ def login_user(request):
 
 @login_required(login_url='/login/')
 def homepage(request):
-    return render(request, 'serapis/homepage.html', {'myuser':request.user})
+    username = request.user
+    user = User.objects.filter(username=username)[0]
+    user_profile = UserProfile.objects.filter(user=user)[0]
+    return render(request, 'serapis/homepage.html', {'user_profile': user_profile, 'myuser': request.user})
 
 
 def registration(request):
-    # if this is a POST request we need to process the form data
     if request.method == 'POST':
-            return HttpResponse("Thanks.")
-    # if a GET (or any other method) we'll create a blank form
+    # if this is a POST request we need to process the form data
+        return HttpResponse("Thanks.")
     else:
+    # if a GET (or any other method) we'll create a blank form
         form = UserCreationForm()
     return render(request, 'main/registration.html', {'form':form})
 
