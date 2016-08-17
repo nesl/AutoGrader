e54a5db4ef1f18a7a93f9129c43dc0259625f548
Bo-Jhang <TimeStringAlpha@gmail.com>
2016-08-17 16:56:46 -0700

    Add view part of the course

--- embed_grader/serapis/views.py
+++ embed_grader/serapis/views.py
@@ -39,5 +39,5 @@ def course(request, course_id):
         return HttpResponse("Course cannot be found")
 
     course = course_list[0]
-    return render(request, 'serapis/homepage.html', {'user_profile': user_profile, 'myuser': request.user})
+    return render(request, 'serapis/course.html', {'myuser': request.user, 'user_profile': user_profile, 'course': course})
 
