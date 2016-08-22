17c3e97c8468948748e77e18d77ed892e74b6ed3
Bo-Jhang <TimeStringAlpha@gmail.com>
2016-08-21 20:54:17 -0700

    Reorder models in models.py

--- embed_grader/serapis/models.py
+++ embed_grader/serapis/models.py
@@ -5,6 +5,92 @@ from django.contrib.auth.models import User
 
 
 
+class UserProfile(models.Model):
+    ROLE_SUPER_USER = 0
+    ROLE_INSTRUCTOR = 10
+    ROLE_TA = 11
+    ROLE_GRADER = 12
+    ROLE_STUDENT = 20
+    USER_ROLES = (
+            (ROLE_SUPER_USER, 'Super user'),
+            (ROLE_INSTRUCTOR, 'Instructor'),
+            (ROLE_TA, 'TA'),
+            (ROLE_GRADER, 'Grader'),
+            (ROLE_STUDENT, 'Student'),
+    )
+    
+    #Connect to built-in User model, which already has firstname, lastname, email and password
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
+
+    #TODO: We may want to use in-built "Groups" feature of Django to make permissions easier
+    #TODO: The role of a user can change from TA to Student depending on a course
+    user_role = models.IntegerField(choices = USER_ROLES, default = ROLE_STUDENT)
+    uid = models.CharField(max_length=20, unique=True, default = '123456789', verbose_name = "University ID")
+
+
+class Course(models.Model):
+    instructor_id = models.ForeignKey(UserProfile, on_delete = models.CASCADE)
+    course_code = models.CharField(max_length = 10, default = '')
+    name = models.CharField(max_length = 100, default = '')
+    description = models.TextField()
+
+
+class Assignment(models.Model):
+    course_id = models.ForeignKey(Course, on_delete = models.CASCADE)
+    # TODO: permission only for instructor
+    description = models.TextField()  # brief
+    # TODO: link to assignment page (do we need this?)
+    release_time = models.DateTimeField()
+    deadline = models.DateTimeField()
+    # TODO: tester type
+    # TODO: DUT type
+    DUT_count = models.IntegerField()
+    # TODO: Wiring information
+    # Testbenches are reserved using AssignmentTestBenches table
+    num_testbenches = models.IntegerField()
+    
+    # TODO: status (what is this?)
+    # TODO: grading script
+
+
+class AssignmentTestBenches(models.Model):
+    assignment_id = models.ForeignKey(Assignment, on_delete = models.CASCADE)
+    testbench_id = models.ForeignKey(HardwareTestBench, on_delete=models.CASCADE)
+
+
+class AssignmentTask(models.Model):
+    assignment_id = models.ForeignKey(Assignment, on_delete = models.CASCADE)
+    task_order = models.IntegerField()
+    description = models.TextField()
+    # TODO: test input
+    # TODO: test output
+    # TODO: points as integer
+    # TODO: mode (public, feedback, hidden)
+
+
+class Submission(models.Model):
+    STAT_RECEIVED = 0
+    STAT_GRADING = 10
+    STAT_REGRADING = 11
+    STAT_GRADED = 20
+    SUBMISSION_STATES = (
+            (STAT_RECEIVED, "Received"),
+            (STAT_GRADING, "Grading"),
+            (STAT_REGRADING, "Rejudging"),
+            (STAT_GRADED, "Final result"),
+    )
+
+    student_id = models.ForeignKey(UserProfile, on_delete = models.CASCADE)
+    assignment_id = models.ForeignKey(Assignment, on_delete = models.CASCADE)
+    submission_time = models.DateTimeField()
+    grading_result = models.FloatField()
+    status = models.IntegerField(choices = SUBMISSION_STATES, default = STAT_RECEIVED)
+
+
+class SubmissionFile(models.Model):
+    submission_id = models.ForeignKey(Submission, on_delete = models.CASCADE)
+    file = models.FileField()
+
 
 class HardwareTestBench(models.Model):
     STATUS_TYPES = (
@@ -94,82 +180,3 @@ class DUTToDUTWiring(models.Model):
 
 
    
-class UserProfile(models.Model):
-    ROLE_SUPER_USER = 0
-    ROLE_INSTRUCTOR = 10
-    ROLE_TA = 11
-    ROLE_GRADER = 12
-    ROLE_STUDENT = 20
-    USER_ROLES = (
-            (ROLE_SUPER_USER, 'Super user'),
-            (ROLE_INSTRUCTOR, 'Instructor'),
-            (ROLE_TA, 'TA'),
-            (ROLE_GRADER, 'Grader'),
-            (ROLE_STUDENT, 'Student'),
-    )
-    
-    #Connect to built-in User model, which already has firstname, lastname, email and password
-    user = models.OneToOneField(User, on_delete = models.CASCADE)
-
-    #TODO: We may want to use in-built "Groups" feature of Django to make permissions easier
-    #TODO: The role of a user can change from TA to Student depending on a course
-    user_role = models.IntegerField(choices = USER_ROLES, default = ROLE_STUDENT)
-    uid = models.CharField(max_length=20, unique=True, default = '123456789', verbose_name = "University ID")
-
-class Course(models.Model):
-    instructor_id = models.ForeignKey(UserProfile, on_delete = models.CASCADE)
-    course_code = models.CharField(max_length = 10, default = '')
-    name = models.CharField(max_length = 100, default = '')
-    description = models.TextField()
-
-class Assignment(models.Model):
-    course_id = models.ForeignKey(Course, on_delete = models.CASCADE)
-    # TODO: permission only for instructor
-    description = models.TextField()  # brief
-    # TODO: link to assignment page (do we need this?)
-    release_time = models.DateTimeField()
-    deadline = models.DateTimeField()
-    # TODO: tester type
-    # TODO: DUT type
-    DUT_count = models.IntegerField()
-    # TODO: Wiring information
-    # Testbenches are reserved using AssignmentTestBenches table
-    num_testbenches = models.IntegerField()
-    
-    # TODO: status (what is this?)
-    # TODO: grading script
-
-class AssignmentTestBenches(models.Model):
-    assignment_id = models.ForeignKey(Assignment, on_delete = models.CASCADE)
-    testbench_id = models.ForeignKey(HardwareTestBench, on_delete=models.CASCADE)
-
-class AssignmentTask(models.Model):
-    assignment_id = models.ForeignKey(Assignment, on_delete = models.CASCADE)
-    task_order = models.IntegerField()
-    description = models.TextField()
-    # TODO: test input
-    # TODO: test output
-    # TODO: points as integer
-    # TODO: mode (public, feedback, hidden)
-
-class Submission(models.Model):
-    STAT_RECEIVED = 0
-    STAT_GRADING = 10
-    STAT_REGRADING = 11
-    STAT_GRADED = 20
-    SUBMISSION_STATES = (
-            (STAT_RECEIVED, "Received"),
-            (STAT_GRADING, "Grading"),
-            (STAT_REGRADING, "Rejudging"),
-            (STAT_GRADED, "Final result"),
-    )
-
-    student_id = models.ForeignKey(UserProfile, on_delete = models.CASCADE)
-    assignment_id = models.ForeignKey(Assignment, on_delete = models.CASCADE)
-    submission_time = models.DateTimeField()
-    grading_result = models.FloatField()
-    status = models.IntegerField(choices = SUBMISSION_STATES, default = STAT_RECEIVED)
-
-class SubmissionFile(models.Model):
-    submission_id = models.ForeignKey(Submission, on_delete = models.CASCADE)
-    file = models.FileField()
