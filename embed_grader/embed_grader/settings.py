91d86b970ff056bbe79756d5033cdb8256952a99
Bo-Jhang <TimeStringAlpha@gmail.com>
2016-08-22 23:33:03 -0700

    Add DateTimeWidget to create-assignment page

--- embed_grader/embed_grader/settings.py
+++ embed_grader/embed_grader/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'datetimewidget',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -49,6 +50,7 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
 ]
 
 ROOT_URLCONF = 'embed_grader.urls'
--- embed_grader/serapis/model_forms.py
+++ embed_grader/serapis/model_forms.py
@@ -14,6 +14,12 @@ class AssignmentForm(ModelForm):
     class Meta:
         model = Assignment
         fields = ['course_id', 'description', 'release_time', 'deadline', 'DUT_count', 'num_testbenches']
+        date_time_options = {
+                'format': 'dd/mm/yyyy HH:ii P',
+                'autoclose': True,
+                'showMeridian' : True,
+        }
         widgets = {
-            'release_time': DateTimeWidget(usel10n = True, bootstrap_version = 3)
+            'release_time': DateTimeWidget(bootstrap_version = 3, options = date_time_options),
+            'deadline': DateTimeWidget(bootstrap_version = 3, options = date_time_options),
         }
