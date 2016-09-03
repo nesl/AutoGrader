703c3a2bb57752f528e6e431751a9715e0b67ca0
guojiaMM <rena63350@gmail.com>
2016-09-02 20:22:39 -0700

    move sidebar leftwards

--- embed_grader/serapis/groups.py
+++ embed_grader/serapis/groups.py
@@ -10,9 +10,9 @@ ct1=ContentType.objects.get(app_label='auth', model='user')
 ct2=ContentType.objects.get(app_label='serapis', model='course')
 ct3=ContentType.objects.get(app_label='serapis', model='assignment')
 
-p1=Permission.objects.get(codename='view_membership', name='Can View Membership', content_type=ct1)
-p2=Permission.objects.get(codename='add_course', name='Can Modify Courses', content_type=ct2)
-p3=Permission.objects.get(codename='add_assignment', name='Can Modify Assignments', content_type=ct3)
+p1=Permission.objects.get(codename='view_membership')
+p2=Permission.objects.get(codename='add_course')
+p3=Permission.objects.get(codename='add_assignment')
 
 g1.permissions.add(p1,p2,p3)
 g2.permissions.add(p1)
