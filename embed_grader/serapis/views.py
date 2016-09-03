f394aa7654668010d4a722fc3634f8e04ac3986b
timestring <timestringalpha@gmail.com>
2016-09-02 19:26:48 -0700

    Get rid of course permission
    - instead, use CourseUserList model

--- embed_grader/serapis/views.py
+++ embed_grader/serapis/views.py
@@ -25,12 +25,7 @@ import hashlib, random
 
 
 #TODO(timestring): recheck whether I should use disabled instead of readonly to enforce data integrity
-def course_check(user, course_id):
-    course_list = Course.objects.filter(id=course_id)
-    if not course_list:
-        return True
-    course = course_list[0]
-    return user.groups.filter(name=course.course_code+'_'+str(course.quarter)+'_'+str(course.year)).exists()
+
 
 def registration(request):
     if request.method == 'POST':
@@ -170,8 +165,9 @@ def modify_course(request, course_id):
 
 @login_required(login_url='/login/')
 def course(request, course_id):
-    if not request.user.has_perm('serapis.add_course'):
-        raise PermissionDenied
+    #TODO: the following permission is wrong
+    #if not request.user.has_perm('serapis.add_course'):
+    #    raise PermissionDenied
     username = request.user
     user = User.objects.filter(username=username)[0]
     user_profile = UserProfile.objects.filter(user=user)[0]
@@ -182,8 +178,10 @@ def course(request, course_id):
     if not course_list:
         return HttpResponse("Course cannot be found")
     course = course_list[0]
-    if not course_check(user, course_id):
+
+    if not CourseUserList.objects.filter(course_id=course, user_id=user):
         raise PermissionDenied
+
     assignment_list = Assignment.objects.filter(course_id=course_id)
     template_context = {
             'myuser': request.user,
@@ -202,12 +200,16 @@ def membership(request, course_id):
     username = request.user
     user = User.objects.filter(username=username)[0]
     user_profile = UserProfile.objects.filter(user=user)[0]
+
     course_list = Course.objects.filter(id=course_id)
     if not course_list:
         return HttpResponse("Course cannot be found")
     course = course_list[0]
-    if not course_check(user, course_id):
+
+    if not CourseUserList.objects.filter(course_id=course, user_id=user):
         raise PermissionDenied
+
+    #TODO: redo the following section section
     user_enrolled = []
     user_list = User.objects.all()
     for u in user_list:
@@ -215,6 +217,7 @@ def membership(request, course_id):
           up = UserProfile.objects.get(user=u)
           user_enrolled.append(up)
     return render(request, 'serapis/roster.html', {'course': course, 'user_enrolled': user_enrolled})
+
 
 @login_required(login_url='/login/')
 def create_assignment(request, course_id):
@@ -228,8 +231,10 @@ def create_assignment(request, course_id):
     if not course_list:
         return HttpResponse("Course cannot be found")
     course = course_list[0]
-    if not course_check(user, course_id):
+
+    if not CourseUserList.objects.filter(course_id=course, user_id=user):
         raise PermissionDenied
+
     if request.method == 'POST':
         form = AssignmentBasicForm(request.POST)
         if form.is_valid():
@@ -253,16 +258,18 @@ def assignment(request, course_id, assignment_id):
     username = request.user
     user = User.objects.filter(username=username)[0]
     user_profile = UserProfile.objects.filter(user=user)[0]
+    
     course_list = Course.objects.filter(id=course_id)
     if not course_list:
         return HttpResponse("Course cannot be found")
     course = course_list[0]
-    if not course_check(user, course_id):
+    
+    if not CourseUserList.objects.filter(course_id=course, user_id=user):
         raise PermissionDenied
+    
     assignment_list = course.assignment_set.filter(id=assignment_id)
     if not assignment_list:
         return HttpResponse("Assignment cannot be found")
-
     assignment = assignment_list[0]
 
     if request.method == 'POST':
@@ -296,16 +303,18 @@ def modify_assignment(request, course_id, assignment_id):
     username = request.user
     user = User.objects.filter(username=username)[0]
     user_profile = UserProfile.objects.filter(user=user)[0]
+    
     course_list = Course.objects.filter(id=course_id)
     if not course_list:
         return HttpResponse("Course cannot be found")
     course = course_list[0]
-    if not course_check(user, course_id):
+    
+    if not CourseUserList.objects.filter(course_id=course, user_id=user):
         raise PermissionDenied
+    
     assignment_list = course.assignment_set.filter(id=assignment_id)
     if not assignment_list:
         return HttpResponse("Assignment cannot be found")
-
     assignment = assignment_list[0]
 
     if request.method == 'POST':
