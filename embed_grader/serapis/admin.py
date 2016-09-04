fa8709b077bf015de47aa4512e9c5d936ed4a178
Bharathan Balaji <bbalaji@ucla.edu>
2016-09-04 16:48:58 -0700

    fixing a bunch of things about UI

--- embed_grader/serapis/admin.py
+++ embed_grader/serapis/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from serapis.models import UserProfile, Course, Assignment
+from django.contrib.auth.models import Permission
 
 # Register your models here.
+#Built-in models
+admin.site.register(Permission)
+
+#Our models
 admin.site.register(UserProfile)
 admin.site.register(Course)
 admin.site.register(Assignment)
