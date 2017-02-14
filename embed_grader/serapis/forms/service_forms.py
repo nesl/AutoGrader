9300f1acb2c40d9d6ac8017509f82835a9d9da0b
Bo-Jhang <TimeStringAlpha@gmail.com>
2017-02-13 22:19:56 -0800

    Make service_forms.py compatible with file_schema

--- embed_grader/serapis/forms/service_forms.py
+++ embed_grader/serapis/forms/service_forms.py
@@ -13,17 +13,21 @@ class ReturnDutOutputForm(forms.Form):
         # services.py has checked the graded task is valid
         task = testbed.task_being_graded
 
-        task_files = file_schema.get_task_grading_status_files(task)
-        for field in task_files:
-            field_name = 'file_' + field
+        schema_names = file_schema.get_task_grading_status_file_schema_names(task)
+        for schema_name in schema_names:
+            field_name = 'file_' + schema_name
             self.fields[field_name] = forms.FileField(allow_empty_file=True)
 
         # set up variables to be used
         self.testbed = testbed
         self.task = task
-        self.task_files = task_files
+        self.schema_names = schema_names
 
     def save_and_commit(self):
+        """
+        Return:
+          (task_grading_status, testbed)
+        """
         now = timezone.now()
 
         self.task.grading_status = TaskGradingStatus.STAT_OUTPUT_TO_BE_CHECKED
@@ -37,12 +41,11 @@ class ReturnDutOutputForm(forms.Form):
         self.testbed.status = Testbed.STATUS_AVAILABLE
         self.testbed.save()
 
-        grading_task_status_files = []
-        for field in self.task_files:
+        schema_name_2_files = {}
+        for schema_name in self.schema_names:
             field_name = 'file_' + field
-            task_file = self.task_files[field]
-            task_file.file = self.cleaned_data[field_name]
-            task_file.save()
-            grading_task_status_files.append(task_file)
+            schema_name_2_files[schema_name] = self.cleaned_data[field_name]
+        file_schema.save_dict_schema_name_to_task_grading_status_files(
+                self.task, schema_name_2_files)
 
-        return (self.task, self.testbed, grading_task_status_files)
+        return (self.task, self.testbed)
