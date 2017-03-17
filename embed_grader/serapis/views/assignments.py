289cd7e725da1083ca9c3856016eb09f722c7ec3
timestring <timestringalpha@gmail.com>
2017-03-16 17:00:05 -0700

    Also show submissions which are grading in the instruction view

--- embed_grader/serapis/views/assignments.py
+++ embed_grader/serapis/views/assignments.py
@@ -96,16 +96,25 @@ def assignment(request, assignment_id):
     submission_form = (AssignmentSubmissionForm(user=user, assignment=assignment)
             if can_submit else None)
 
-    if user.has_perm('modify_assignment', course):
+    submission_lists = {}
+    if not user.has_perm('modify_assignment', course):
+        submission_lists['student'] = Submission.objects.filter(
+                student_id=user, assignment_id=assignment).order_by('-id')[:10]
+    else:
         students = [o.user_id for o in CourseUserList.objects.filter(course_id=course)]
-        submission_list = []
+        graded_list = []
+        grading_list = []
         for student in students:
             sub = grading.get_last_fully_graded_submission(student, assignment)
             if sub:
-                submission_list.append(sub)
-    else:
-        submission_list = Submission.objects.filter(
-                student_id=user, assignment_id=assignment).order_by('-id')[:10]
+                graded_list.append(sub)
+            
+            sub = grading.get_last_grading_submission(student, assignment)
+            if sub:
+                grading_list.append(sub)
+
+        submission_lists['graded'] = graded_list
+        submission_lists['grading'] = grading_list
 
     is_deadline_passed = assignment.is_deadline_passed()
     enrollment, contributors, score_statistics = score_distribution.get_class_statistics(
@@ -120,7 +129,7 @@ def assignment(request, assignment_id):
             'assignment': assignment,
             'submission_form': submission_form,
             'reason_of_cannot_submit': reason_of_cannot_submit,
-            'submission_list': submission_list,
+            'submission_lists': submission_lists,
             'assignment_tasks': assignment_tasks_with_file_list,
             'public_points': public_points,
             'total_points': total_points,
