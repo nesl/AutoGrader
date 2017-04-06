868d7e168c22d6886ee1805b859cfdb8493d5ad0
Bo-Jhang <TimeStringAlpha@gmail.com>
2017-04-05 18:16:39 -0700

    Remove tabs to blank spaces

--- embed_grader/serapis/utils/send_mail_helper.py
+++ embed_grader/serapis/utils/send_mail_helper.py
@@ -4,12 +4,12 @@ from django.core.mail import send_mail
 
 
 def send_by_template(subject, recipient_email_list, template_path, context_dict,
-		fail_silently=False):
+        fail_silently=False):
     template = get_template(template_path)
     context = Context(context_dict)
     message = template.render(context)
     send_mail(
-			subject=subject,
+            subject=subject,
             message=message,
             from_email='NESL Embed AutoGrader',
             recipient_list=recipient_email_list,
