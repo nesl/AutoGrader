f39919671971e20917df64f54a93e61f6a2932eb
Xi Han <xihan94@ucla.edu>
2017-05-31 14:53:06 -0700

    Adjust tests

--- embed_grader/serapis/tests/test_user.py
+++ embed_grader/serapis/tests/test_user.py
@@ -16,6 +16,9 @@ class UserTestCase(TestCase):
         defaultUser.is_active = True
         defaultUser.save()
 
+        self.defaultUser = defaultUser
+        self.defaultUserProfile = defaultUserProfile
+
 
     def test_creating_user_with_duplicate_username_should_fail(self):
         self.assertRaises(IntegrityError, User.objects.create_user, 'xihan94', email='xihanmeow@ucla.edu', password='biubiubiu')
@@ -25,4 +28,30 @@ class UserTestCase(TestCase):
 
     # These checks are done in form level
     # def test_creating_user_with_duplicate_email_should_fail(self):
-    # def test_creating_user_with_empty_email_should_fail(self):  
+    # def test_creating_user_with_empty_email_should_fail(self):
+
+    def test_creating_user_profile_with_none_user_object_should_fail(self):
+        anotherUserProfile = UserProfile(user=None, uid='135798642')
+        self.assertRaises(IntegrityError, anotherUserProfile.save)
+
+    def test_creating_user_profile_with_none_uid_should_fail(self):
+        anotherUser = User.objects.create_user('dummyuser', email='dummyuser@ucla.edu', password='biubiubiu');
+        anotherUser.first_name = 'Dummy'
+        anotherUser.last_name = 'User'
+        anotherUser.save()
+
+        anotherUserProfile = UserProfile(user=anotherUser, uid=None)
+        self.assertRaises(IntegrityError, anotherUserProfile.save)
+
+    def test_linking_multiple_profiles_to_a_user_should_fail(self):
+        anotherUserProfile = UserProfile(user=self.defaultUser, uid='135798642')
+        self.assertRaises(IntegrityError, anotherUserProfile.save)
+
+    def test_creating_user_with_duplicate_uid_should_fail(self):
+        anotherUser = User.objects.create_user('dummyuser', email='dummyuser@ucla.edu', password='biubiubiu');
+        anotherUser.first_name = 'Dummy'
+        anotherUser.last_name = 'User'
+        anotherUser.save()
+
+        anotherUserProfile = UserProfile(user=anotherUser, uid='504136747')
+        self.assertRaises(IntegrityError, anotherUserProfile.save)
