710bdb8fe9e438113cb4a9d75c56ee3a83444100
NESL Embed Autograder <embed_grader@nesl.ucla.edu>
2017-10-06 15:27:00 -0700

    Fix the issue that submission form error will go silent

--- embed_grader/serapis/utils/user_info_helper.py
+++ embed_grader/serapis/utils/user_info_helper.py
@@ -1,7 +1,5 @@
 from serapis.models import *
 
-from serapis.utils import team_helper
-
 
 def get_first_last_name(user):
     return user.first_name + ' ' + user.last_name
@@ -10,7 +8,9 @@ def get_last_first_name(user):
     return user.last_name + ' ' + user.first_name
 
 def all_submission_graded_on_assignment(user, assignment):
-    team = team_helper.get_belonged_team(user, assignment)
+    # get belonged team
+    member_list = TeamMember.objects.filter(user_fk=user, team_fk__assignment_fk=assignment)
+    team = member_list[0].team_fk if member_list else None
     query = Submission.objects.filter(team_fk=team, assignment_fk=assignment)
     if query.count() == 0:
         return True
